f8bab95422b7380f12838117d9134c2044eb45fb
Kevin Wortman <kwortman@fullerton.edu>
2022-03-15 11:26:22 -0700

    workaround for glibc bug

--- autograder-skeleton/setup.sh
+++ autograder-skeleton/setup.sh
@@ -1,3 +1,15 @@
+#! /bin/bash
+
+# GradeScope tech support workaround for glibc bug
+# (https://bugs.launchpad.net/ubuntu/+source/glibc/+bug/1962606)
+mv /bin/uname /bin/uname.orig
+printf '#!/bin/bash\n\nif [[ "$1" == "-r" ]] ;then\n echo '4.9.250'\n exit\nelse\n uname.orig "$@"\nfi' > /bin/uname
+chmod 755 /bin/uname
+
+# update packages
+apt-get -y update
+apt-get -y upgrade
+apt-get -y dist-upgrade
 
 # googletest
 apt-get -y install build-essential cmake libgtest-dev
